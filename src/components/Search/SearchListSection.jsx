cd0c50f98b2fabe7676679f8064a57d076cd2e93
jyejyes <p_jihye97@naver.com>
2022-03-29 10:08:17 +0900

    :bug: :: #11 search 버그 수정
    data 가 들어오지 않으면 length 오류가 나는 문제 해결

--- src/components/Search/SearchListSection.jsx
+++ src/components/Search/SearchListSection.jsx
@@ -43,7 +43,10 @@ const SearchListSection = () => {
       <WrapStyle>
         <div className="list-section">
           <div className="info-text-section">
-            <p>지도에 표시된 지역에 위치한 ${num}개 이상의 숙소</p>
+            <p>
+              지도에 표시된 지역에 위치한 {datas ? `${datas.length}` : "0"}개
+              이상의 숙소
+            </p>
             <p>
               여행 날짜와 게스트 인원수를 입력하면 1박당 총 요금을 확인할 수
               있습니다.
@@ -55,26 +58,30 @@ const SearchListSection = () => {
           </div>
 
           <div className="item-section">
-            {datas.map((item, index) => (
-              <SearchItem
-                key={index}
-                houseImg={item.houseImg}
-                gu={item.gu}
-                bigType={item.bigType}
-                smallType={item.smallType}
-                houseName={item.houseName}
-                reviewStar={item.reviewStar}
-                reviewCount={item.reviewCount}
-                houseGuest={item.houseGuest}
-                houseBed={item.houseBed}
-                houseRoom={item.houseRoom}
-                houseBath={item.houseBath}
-                housePrice={item.housePrice}
-                totalPrice={item.totalPrice}
-              />
-            ))}
+            {!datas ? (
+              <p className="non-search">검색 결과가 없습니다.</p>
+            ) : (
+              datas.map((item, index) => (
+                <SearchItem
+                  key={index}
+                  houseImg={item.houseImg}
+                  gu={item.gu}
+                  bigType={item.bigType}
+                  smallType={item.smallType}
+                  houseName={item.houseName}
+                  reviewStar={item.reviewStar}
+                  reviewCount={item.reviewCount}
+                  houseGuest={item.houseGuest}
+                  houseBed={item.houseBed}
+                  houseRoom={item.houseRoom}
+                  houseBath={item.houseBath}
+                  housePrice={item.housePrice}
+                  totalPrice={item.totalPrice}
+                />
+              ))
+            )}
+
             {/* 필요한거 : houseImg, gu, bigType, smallType, houseName, reviewStar, reviewCount, houseGuest, houseBed, houseRoom,ㄴhouseBath,  */}
-            {/* <SearchItem /> */}
           </div>
         </div>
       </WrapStyle>
@@ -106,6 +113,15 @@ const WrapStyle = styled.section`
       &:nth-child(2) {
         color: ${color.medium_gray2};
       }
+    }
+  }
+
+  .item-section {
+    .non-search {
+      text-align: center;
+      font-size: 1.6rem;
+      color: ${color.medium_gray2};
+      margin-top: 10rem;
     }
   }
 `;
