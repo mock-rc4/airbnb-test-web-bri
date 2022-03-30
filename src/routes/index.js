5076e1ff5219154edb38ec0b326671f43d6d297f
jyejyes <p_jihye97@naver.com>
2022-03-31 02:25:37 +0900

    :memo: :: #13 router page 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,10 @@ import RoomContentPage from "../pages/Hosting/RoomContentPage";
 import RoomPricePage from "../pages/Hosting/RoomPrice";
 import ConfirmPage from "../pages/Hosting/ConfirmPage";
 import CompletePage from "../pages/Hosting/CompletePage";
+import PaymentPage from "../pages/PaymentPage";
+import TripPage from "../pages/TripPage";
+import ManagementPage from "../pages/ManagementPage";
+import ReservationPage from "../pages/ReservationPage";
 
 const RootRoute = () => {
   return (
@@ -26,7 +30,11 @@ const RootRoute = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/test" element={<Test />} />
-        <Route path="/detail" element={<DetailPage />} />
+        {/* detail , 예약페이지 관련 */}
+        <Route path="/detail/:houseIdx" element={<DetailPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/reservation" element={<ReservationPage />} />
+        {/* hosting */}
         <Route path="/hosting" element={<HostingStartPage />} />
         <Route path="/hosting/property" element={<PropertyTypePage />} />
         <Route path="/hosting/privacy" element={<PrivacyTypePage />} />
@@ -38,6 +46,11 @@ const RootRoute = () => {
         <Route path="/hosting/roomprice" element={<RoomPricePage />} />
         <Route path="/hosting/confirm" element={<ConfirmPage />} />
         <Route path="/hosting/complete" element={<CompletePage />} />
+        {/* trip(예약확인) */}
+        <Route path="/trip" element={<TripPage />} />
+        {/* wishlist */}
+        <Route path="/management" element={<ManagementPage />} />
+        {/* setting */}
         <Route path="/setting" element={<SettingPage />} />
         <Route path="/setting/personal" element={<PersonalPage />} />
       </Routes>
